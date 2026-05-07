b524d0bced106d2ec026bdf777f79aa5ee68f600
Nandini2807 <nandinijadhav2812@gmail.com>
2026-05-07 14:45:59 +0530

    prototype

---
+++ Day-06/prototype.js
@@ -0,0 +1,12 @@
+function person(name){
+    this.name = name;
+}
+person.prototype.sayHi=function(){
+    console.log ('hello' + this.name);
+}
+const me = new person("nandini");
+const you = new person("you");
+me.sayHi();
+you.sayHi();
+console.log(Object.getPrototypeOf(me)=== person.prototype)
+console.log(me.sayHi === you.sayHi)

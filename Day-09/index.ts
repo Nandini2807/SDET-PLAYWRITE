cc808b604f14df3dbbd224debcff56ff12406572
Nandini2807 <nandinijadhav2812@gmail.com>
2026-05-11 12:45:48 +0530

    Basic TypeScript

---
+++ Day-09/index.ts
@@ -0,0 +1,11 @@
+let name :string = "Nandini";
+let result : number | string;
+result = 50;
+result = "success"    //updated value will print
+
+console.log(name, result);
+
+function add(a: number, b:number):string {
+    return "Sum:" +(a+b);
+}
+console.log(add(5,10));
